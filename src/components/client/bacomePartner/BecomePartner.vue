19ddd11a7a7e037ccce85886263529d33acc0371
nadiaH <nadiahrb79@gmailcom>
2022-10-24 03:53:04 +0300

    wave line col attempt

--- src/components/client/bacomePartner/BecomePartner.vue
+++ src/components/client/bacomePartner/BecomePartner.vue
@@ -1,8 +1,5 @@
 <template>
-  <section
-    class="relative"
-    style="background-image: url('images/waveLine.svg'), url('images/colorLine.svg')"
-  >
+  <section class="relative">
     <div class="w-75 relative_div">
       <h2 class="fw-bold">
         Choicie for cafes and restaurants.<br />Five growth drivers for your development
@@ -37,12 +34,32 @@
         Become a Choicie partner!
       </b-button>
     </div>
-    <section
-      class="vw-100 absolute"
-      style="
-        background-image: url('images/dashedLine.svg'), url('images/verticalLine.svg');
-      "
-    ></section>
+    <div class="absolute d-flex gap-0">
+      <b-row class="w-100">
+        <b-col cols="8" class="p-0">
+          <WaveLine class="w-100" />
+        </b-col>
+        <b-col cols="4" class="p-0 d-flex position-relative">
+          <!-- <b-col> -->
+          <VerticalLine class="vertical" />
+          <!-- </b-col>
+          <b-col> -->
+          <DashedLine class="w-100" />
+          <EllipseYellow class="position-absolyte ellipse" />
+          <!-- </b-col> -->
+        </b-col>
+      </b-row>
+      <!-- <div class="w-100 position-relative wave">
+      <WaveLine class="" />
+        </div>
+        <div>
+        <ColorLine class="position-absolute wave__color" />
+      </div>
+      <div class="w-25 position-relative">
+        </div>
+        <div>
+      </div> -->
+    </div>
   </section>
 </template>
 
@@ -52,13 +69,19 @@ import Reputation from "../../icons/Reputation.vue";
 import Scan from "../../icons/Scan.vue";
 import UserProfile from "../../icons/UserProfile.vue";
 import Happy from "../../icons/Happy.vue";
+import WaveLine from "../../icons/WaveLine.vue";
+import ColorLine from "../../icons/ColorLine.vue";
+import VerticalLine from "../../icons/VerticalLine.vue";
+import DashedLine from "../../icons/DashedLine.vue";
+// import EllipseWhite from "../../icons/EllipseWhite.vue";
+import EllipseYellow from "../../icons/EllipseYellow.vue";
 </script>
 
 <style scoped lang="scss">
 .relative {
   position: relative;
   padding-top: 2rem;
-  padding-bottom: 19rem;
+  padding-bottom: 22rem;
   background-color: transparent;
   background-repeat: no-repeat, no-repeat;
   background-position: 0em 20em, 0em 21em;
@@ -76,13 +99,50 @@ import Happy from "../../icons/Happy.vue";
 }
 
 .absolute {
+  width: 100%;
   height: 100%;
   position: absolute;
-  top: 0em;
+  bottom: -22em;
   left: 0em;
   background-repeat: no-repeat, no-repeat;
   background-position: 62em 18em, 62em 27em;
   background-size: 30% 20%, auto 70%;
-  z-index: -1;
+  // z-index: -1;
+}
+
+.wave {
+  &__wave {
+    width: 100%;
+    bottom: 2%;
+    left: 0;
+  }
+
+  &__color {
+    width: 100%;
+    bottom: 0;
+    left: 0;
+  }
+
+  &__vertical {
+    // height: 55%;
+    bottom: -8%;
+    left: 0;
+  }
+
+  &__dashed {
+    width: 100%;
+    bottom: 62%;
+    right: 0;
+  }
+}
+
+.vertical {
+  height: 50%;
+  margin-top: 100px;
+}
+
+.ellipse {
+  top: 0;
+  left: 100%;
 }
 </style>
--- src/components/icons/EllipseYellow.vue
+++ src/components/icons/EllipseYellow.vue
@@ -1,9 +1,24 @@
-<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="16" cy="16" r="16" fill="url(#paint0_radial_160_1846)"/>
-<defs>
-<radialGradient id="paint0_radial_160_1846" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(19 13) rotate(116.565) scale(17.8885)">
-<stop stop-color="#F9D990"/>
-<stop offset="1" stop-color="#E5C270"/>
-</radialGradient>
-</defs>
-</svg>
+<template>
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="16" cy="16" r="16" fill="url(#paint0_radial_160_1846)" />
+    <defs>
+      <radialGradient
+        id="paint0_radial_160_1846"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(19 13) rotate(116.565) scale(17.8885)"
+      >
+        <stop stop-color="#F9D990" />
+        <stop offset="1" stop-color="#E5C270" />
+      </radialGradient>
+    </defs>
+  </svg>
+</template>
