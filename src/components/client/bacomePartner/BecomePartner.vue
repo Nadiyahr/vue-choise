f70878ecc81cf0219beeb5377036790883c77123
nadiaH <nadiahrb79@gmailcom>
2022-10-24 14:26:28 +0300

    wave svg and become partner section

--- src/components/client/bacomePartner/BecomePartner.vue
+++ src/components/client/bacomePartner/BecomePartner.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="relative">
-    <div class="w-75 relative_div">
-      <h2 class="fw-bold">
+    <div class="relative__container">
+      <h2 class="fw-bold pb-2 lh-sm">
         Choicie for cafes and restaurants.<br />Five growth drivers for your development
       </h2>
       <p class="fs-6 text-prymary-light">
@@ -9,92 +9,81 @@
       </p>
       <div class="d-flex flex-wrap py-3">
         <div class="d-flex align-items-center gap-3 py-2 pe-4 m-2">
-          <Qualyty />
-          <p class="fs-7 text">Quality of Service</p>
+          <Quality />
+          <p class="fs-7 text fw-bold">Quality of Service</p>
         </div>
         <div class="d-flex align-items-center gap-3 py-2 pe-4 m-2">
           <Reputation />
-          <p class="fs-7 text">Reputation and promotion on the web</p>
+          <p class="fs-7 text fw-bold">Reputation and promotion on the web</p>
         </div>
         <div class="d-flex align-items-center gap-3 py-2 pe-4 m-2">
           <Scan />
-          <p class="fs-7 text">Visibility</p>
+          <p class="fs-7 text fw-bold">Visibility</p>
         </div>
-        <div class="d-flex align-items-center gap-3 py-2 pe-4 m-2">
+        <div class="d-flex align-items-center gap-3 lg-2 pe-4 m-2">
           <UserProfile />
-          <p class="fs-7 text">Number of orders</p>
+          <p class="fs-7 text fw-bold">Number of orders</p>
         </div>
         <div class="d-flex align-items-center gap-3 py-2 pe-4 m-2">
           <Happy />
-          <p class="fs-7 text">Convenience of work with clients</p>
+          <p class="fs-7 text fw-bold">Convenience of work with clients</p>
         </div>
       </div>
-      <h4 class="relative_h">What Does it Take?</h4>
+      <h4 class="relative__title">What Does it Take?</h4>
       <b-button pill size="md" variant="primary" class="relative_btn">
         Become a Choicie partner!
       </b-button>
     </div>
-    <div class="absolute d-flex gap-0">
-      <b-row class="w-100">
-        <b-col cols="8" class="p-0">
-          <WaveLine class="w-100" />
-        </b-col>
-        <b-col cols="4" class="p-0 d-flex position-relative">
-          <!-- <b-col> -->
-          <VerticalLine class="vertical" />
-          <!-- </b-col>
-          <b-col> -->
-          <DashedLine class="w-100" />
-          <EllipseYellow class="position-absolyte ellipse" />
-          <!-- </b-col> -->
-        </b-col>
-      </b-row>
-      <!-- <div class="w-100 position-relative wave">
-      <WaveLine class="" />
-        </div>
-        <div>
-        <ColorLine class="position-absolute wave__color" />
-      </div>
-      <div class="w-25 position-relative">
-        </div>
-        <div>
-      </div> -->
+    <div class="absolute">
+      <WaveLine class="w-100" />
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import Qualyty from "../../icons/Qualyty.vue";
+import Quality from "../../icons/Quality.vue";
 import Reputation from "../../icons/Reputation.vue";
 import Scan from "../../icons/Scan.vue";
 import UserProfile from "../../icons/UserProfile.vue";
 import Happy from "../../icons/Happy.vue";
 import WaveLine from "../../icons/WaveLine.vue";
-import ColorLine from "../../icons/ColorLine.vue";
-import VerticalLine from "../../icons/VerticalLine.vue";
-import DashedLine from "../../icons/DashedLine.vue";
-// import EllipseWhite from "../../icons/EllipseWhite.vue";
-import EllipseYellow from "../../icons/EllipseYellow.vue";
 </script>
 
 <style scoped lang="scss">
 .relative {
   position: relative;
   padding-top: 2rem;
-  padding-bottom: 22rem;
+  padding-bottom: 20rem;
   background-color: transparent;
-  background-repeat: no-repeat, no-repeat;
-  background-position: 0em 20em, 0em 21em;
-  background-size: 70% 70%, 70% 70%;
 
-  &_h {
+  @media (max-width: 600px) {
+    padding: 2rem 1.25rem 20rem;
+  }
+
+  &__container {
+    width: 75%;
+
+    @media (max-width: 600px) {
+      width: 100%;
+    }
+  }
+
+  &__title {
     margin-top: 3rem;
+
+    @media (max-width: 600px) {
+      margin-bottom: 2rem;
+    }
   }
 
   &_btn {
     width: 19rem;
     height: 3.2rem;
-    margin-top: 2.3rem;
+    margin-top: 2rem;
+
+    @media (max-width: 600px) {
+      width: 90%;
+    }
   }
 }
 
@@ -102,47 +91,12 @@ import EllipseYellow from "../../icons/EllipseYellow.vue";
   width: 100%;
   height: 100%;
   position: absolute;
-  bottom: -22em;
+  bottom: -20em;
   left: 0em;
-  background-repeat: no-repeat, no-repeat;
-  background-position: 62em 18em, 62em 27em;
-  background-size: 30% 20%, auto 70%;
-  // z-index: -1;
-}
+  z-index: -1;
 
-.wave {
-  &__wave {
-    width: 100%;
-    bottom: 2%;
-    left: 0;
+  @media (max-width: 600px) {
+    bottom: -45em;
   }
-
-  &__color {
-    width: 100%;
-    bottom: 0;
-    left: 0;
-  }
-
-  &__vertical {
-    // height: 55%;
-    bottom: -8%;
-    left: 0;
-  }
-
-  &__dashed {
-    width: 100%;
-    bottom: 62%;
-    right: 0;
-  }
-}
-
-.vertical {
-  height: 50%;
-  margin-top: 100px;
-}
-
-.ellipse {
-  top: 0;
-  left: 100%;
 }
 </style>
--- src/components/icons/WaveLine.vue
+++ src/components/icons/WaveLine.vue
@@ -1,40 +1,181 @@
 <template>
   <svg
-    width="1242"
-    height="567"
-    viewBox="0 0 1242 567"
+    width="1642"
+    height="667"
+    viewBox="0 0 1642 667"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path
-      d="M-82 546C690.207 648.241 891.387 202.781 1239.5 4.84961"
-      stroke="#F2CF7E"
-      stroke-width="10"
-    />
-    <svg
-      width="1239"
-      height="589"
-      viewBox="0 0 1239 589"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg width="1242" height="567" viewBox="0 0 1242 567">
       <path
-        opacity="0.2"
-        d="M-82 540.581V588.081H1238.5V0C891 198.215 689.468 642.724 -82 540.581Z"
-        fill="url(#paint0_linear_160_1844)"
+        d="M-82 546C690.207 648.241 891.387 202.781 1239.5 4.84961"
+        stroke="#F2CF7E"
+        stroke-width="10"
+      />
+    </svg>
+    <path
+      opacity="0.2"
+      d="M-82 540.581V588.081H1238.5V0C891 198.215 689.468 642.724 -82 540.581Z"
+      fill="url(#paint0_linear_160_1844)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_160_1844"
+        x1="836.25"
+        y1="-91.1709"
+        x2="836.25"
+        y2="588.081"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#F9D990" />
+        <stop offset="1" stop-color="#F9D990" stop-opacity="0" />
+      </linearGradient>
+    </defs>
+    <svg x="1239">
+      <line
+        x1="2"
+        y1="8.74228e-08"
+        x2="1.99997"
+        y2="589"
+        stroke="url(#paint0_linear_160_1851)"
+        stroke-width="4"
       />
       <defs>
         <linearGradient
-          id="paint0_linear_160_1844"
-          x1="836.25"
-          y1="-91.1709"
-          x2="836.25"
-          y2="588.081"
+          id="paint0_linear_160_1851"
+          x1="0"
+          y1="0"
+          x2="-1.71728e-09"
+          y2="589"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#F9D990" />
-          <stop offset="1" stop-color="#F9D990" stop-opacity="0" />
+          <stop stop-color="#F2CF7E" />
+          <stop offset="1" stop-color="white" stop-opacity="0" />
         </linearGradient>
+      </defs>
+    </svg>
+    <svg x="1256" y="-100">
+      <path
+        d="M514.5 23C293.831 -21.3846 136.814 17.3959 2 93.4339"
+        stroke="#ECECEC"
+        stroke-width="8"
+        stroke-dasharray="20 8"
+      />
+    </svg>
+    <svg x="1144" y="-69">
+      <g filter="url(#filter0_d_160_1845)">
+        <circle cx="100" cy="70" r="40" fill="white" />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_160_1845"
+          x="0"
+          y="0"
+          width="200"
+          height="200"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="30" />
+          <feGaussianBlur stdDeviation="30" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.0818069 0 0 0 0 0.0408333 0 0 0 0 0.2 0 0 0 0.09 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_160_1845"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_160_1845"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+    <svg x="1228" y="-14">
+      <circle cx="16" cy="16" r="16" fill="url(#paint0_radial_160_1846)" />
+      <defs>
+        <radialGradient
+          id="paint0_radial_160_1846"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(19 13) rotate(116.565) scale(17.8885)"
+        >
+          <stop stop-color="#F9D990" />
+          <stop offset="1" stop-color="#E5C270" />
+        </radialGradient>
+      </defs>
+    </svg>
+    <svg x="115" y="502">
+      <g filter="url(#filter0_d_160_1847)">
+        <circle cx="85" cy="55" r="25" fill="white" />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_160_1847"
+          x="0"
+          y="0"
+          width="170"
+          height="170"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="30" />
+          <feGaussianBlur stdDeviation="30" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.0818069 0 0 0 0 0.0408333 0 0 0 0 0.2 0 0 0 0.09 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_160_1847"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_160_1847"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+    <svg x="188.5" y="546">
+      <circle cx="11.5" cy="11.5" r="11.5" fill="url(#paint0_radial_160_1848)" />
+      <defs>
+        <radialGradient
+          id="paint0_radial_160_1848"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(13.6563 9.34375) rotate(116.565) scale(12.8574)"
+        >
+          <stop stop-color="#F9D990" />
+          <stop offset="1" stop-color="#E5C270" />
+        </radialGradient>
       </defs>
     </svg>
   </svg>
