e9453752fb9a1f65f7d6bde5d5e1f6c152686d0b
nadiaH <nadiahrb79@gmailcom>
2022-11-05 23:34:55 +0200

    del som import

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,12 @@
 <template>
   <Layout>
     <AboutTop />
-    <!-- <Calc /> -->
   </Layout>
 </template>
 
 <script setup lang="ts">
 import AboutTop from "@/components/about/AboutTop.vue";
 import Layout from "@/components/layout/index.vue";
-import Calc from "@/components/about/Calc.vue";
-
 </script>
 
 <style scoped></style>
